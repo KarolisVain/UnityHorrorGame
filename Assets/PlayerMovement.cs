a40966c7e108728115d316929ea7ef8828a5558f
BroofTerr <lukaitis.rytis@gmail.com>
2021-02-13 18:16:49 +0200

    Camera is now linked with the player object
    When player moves the mouse, it accordingally rotates both: the player object and the camera

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,18 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 8f;
 
+    [Min(0f)]
+    [SerializeField]
+    private float lookSensitivity = 5f;
+
+    [Min(0f)]
+    [SerializeField]
+    private float maxYangle = 80f;
+
+    [SerializeField]
+    private GameObject camera;
+    private Transform cameraT;
+
     private Rigidbody rb;
 
     private Vector3 movementAxis;
@@ -17,23 +29,33 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        camera = gameObject.transform.Find("PlayerCamera").gameObject;
+        cameraT = camera.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         UpdateMovementAxis();
+        UpdateRotationAxis();
     }
 
     private void FixedUpdate()
     {
         UpdatePosition();
+        UpdateRotation();
     }
 
     private void UpdateMovementAxis()
     {
         movementAxis.x = Input.GetAxis("Horizontal");
         movementAxis.z = Input.GetAxis("Vertical");
+    }
+
+    private void UpdateRotationAxis()
+    {
+        rotationAxis.x += Input.GetAxis("Mouse X") * lookSensitivity;
+        rotationAxis.y -= Input.GetAxis("Mouse Y") * lookSensitivity;
     }
 
     private void UpdatePosition()
@@ -44,5 +66,25 @@ public class PlayerMovement : MonoBehaviour
         var newPos = currentPos + posMovement;
 
         rb.MovePosition(newPos);
+    }
+
+    private void UpdateRotation()
+    {
+        // Player Rotation Code
+
+        var playerTransform = gameObject.transform;
+        var rotationMovement = movementAxis.x * Time.deltaTime;
+
+        rotationAxis.x = Mathf.Repeat(rotationAxis.x, 360);
+
+        playerTransform.rotation = Quaternion.Euler(0, rotationAxis.x, 0);
+
+        // Camera Rotation Code
+
+        var cameraTransform = camera.transform;
+
+        rotationAxis.y = Mathf.Clamp(rotationAxis.y, -maxYangle, maxYangle);
+
+        cameraTransform.rotation = Quaternion.Euler(rotationAxis.y, rotationAxis.x, 0);
     }
 }
