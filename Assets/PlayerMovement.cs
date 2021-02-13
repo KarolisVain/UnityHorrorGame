65c8debfe14499857fc61388d2aa1b5a1bde36af
Karolis <Ka.vainauskas@gmail.com>
2021-02-13 23:17:31 +0200

    1
    1

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Min(0f)]
+    [SerializeField]
+    private float moveSpeed = 8f;
+
+    [Min(0f)]
+    [SerializeField]
+    private float lookSensitivity = 5f;
+
+    [Min(0f)]
+    [SerializeField]
+    private float maxYangle = 60f;
+
+    [Min(0f)]
+    [SerializeField]
+    private float maxZangle = 30f;
+
+    [SerializeField]
+    private GameObject camera;
+
+    private Rigidbody rb;
+
+    private Vector3 movementAxis;
+    private Vector3 rotationAxis;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        camera = gameObject.transform.Find("PlayerCamera").gameObject;
+    }
+
+    private void Update()
+    {
+        UpdateMovementAxis();
+        UpdateRotationAxis();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePosition();
+        UpdateRotation();
+    }
+
+    private void UpdateMovementAxis()
+    {
+        movementAxis.x = Input.GetAxis("Horizontal");
+        movementAxis.z = Input.GetAxis("Vertical");
+    }
+
+    private void UpdateRotationAxis()
+    {
+        rotationAxis.x += Input.GetAxis("Mouse X") * lookSensitivity;
+        rotationAxis.y -= Input.GetAxis("Mouse Y") * lookSensitivity;
+
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E))
+        {
+            rotationAxis.z -= Input.GetAxis("Tilt") * lookSensitivity;
+        }
+        else
+        {
+            rotationAxis.z = 0;
+        }
+    }
+
+    private void UpdatePosition()
+    {
+        var posMovement = movementAxis * moveSpeed * Time.deltaTime;
+
+        transform.position += transform.TransformDirection(posMovement);
+    }
+
+    private void UpdateRotation()
+    {
+        // Player Rotation Part
+
+        var playerTransform = gameObject.transform;
+
+        rotationAxis.x = Mathf.Repeat(rotationAxis.x, 360);
+
+        playerTransform.rotation = Quaternion.Euler(0, rotationAxis.x, 0);
+
+        // Camera Rotation Part
+
+        var cameraTransform = camera.transform;
+
+        rotationAxis.y = Mathf.Clamp(rotationAxis.y, -maxYangle, maxYangle);
+        rotationAxis.z = Mathf.Clamp(rotationAxis.z, -maxZangle, maxZangle);
+
+        cameraTransform.rotation = Quaternion.Euler(rotationAxis.y, rotationAxis.x, rotationAxis.z);
+    }
+}
