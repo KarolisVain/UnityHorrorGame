d8f3c3f3071c630f2e12965cc377d83567188ddd
BroofTerr <lukaitis.rytis@gmail.com>
2021-02-13 13:47:40 +0200

    Added simple movement
    Object can now move on the XZ plane.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Min(0f)]
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private Rigidbody rb;
+
+    private Vector3 movementAxis;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        UpdateMovementAxis();
+    }
+
+    private void FixedUpdate()
+    {
+        UpdatePosition();
+    }
+
+    private void UpdateMovementAxis()
+    {
+        movementAxis.x = Input.GetAxis("Horizontal");
+        movementAxis.z = Input.GetAxis("Vertical");
+    }
+
+    private void UpdatePosition()
+    {
+        var posMovement = movementAxis * moveSpeed * Time.deltaTime;
+
+        var currentPos = rb.position;
+        var newPos = currentPos + posMovement;
+
+        Debug.Log(posMovement);
+
+        rb.MovePosition(newPos);
+    }
+}
