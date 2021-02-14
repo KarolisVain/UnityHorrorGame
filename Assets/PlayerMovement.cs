3848e1f1dfb43f0c4b1844c6fdd7e06f4a3a6d07
BroofTerr <lukaitis.rytis@gmail.com>
2021-02-14 20:19:26 +0200

    Revert "Added camera tilt"
    This reverts commit 9b646d3e66a22c61318c775319ae4d0cd01c6a8a.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,11 +14,7 @@ public class PlayerMovement : MonoBehaviour
 
     [Min(0f)]
     [SerializeField]
-    private float maxYangle = 60f;
-
-    [Min(0f)]
-    [SerializeField]
-    private float maxZangle = 30f;
+    private float maxYangle = 80f;
 
     [SerializeField]
     private GameObject camera;
@@ -56,15 +52,6 @@ public class PlayerMovement : MonoBehaviour
     {
         rotationAxis.x += Input.GetAxis("Mouse X") * lookSensitivity;
         rotationAxis.y -= Input.GetAxis("Mouse Y") * lookSensitivity;
-
-        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E))
-        {
-            rotationAxis.z -= Input.GetAxis("Tilt") * lookSensitivity;
-        }
-        else
-        {
-            rotationAxis.z = 0;
-        }
     }
 
     private void UpdatePosition()
@@ -89,8 +76,7 @@ public class PlayerMovement : MonoBehaviour
         var cameraTransform = camera.transform;
 
         rotationAxis.y = Mathf.Clamp(rotationAxis.y, -maxYangle, maxYangle);
-        rotationAxis.z = Mathf.Clamp(rotationAxis.z, -maxZangle, maxZangle);
 
-        cameraTransform.rotation = Quaternion.Euler(rotationAxis.y, rotationAxis.x, rotationAxis.z);
+        cameraTransform.rotation = Quaternion.Euler(rotationAxis.y, rotationAxis.x, 0);
     }
 }
